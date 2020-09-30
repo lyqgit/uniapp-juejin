ef92927b83a0ed88ff1612d009f612dcddc40d9d
King <zhangkjshare@163.com>
2020-09-30 15:19:20 +0800

    切换登录方式

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -8,34 +8,56 @@
 			<image src="../../static/logo.png" mode="aspectFit"></image>
 		</view>
 		<view class="bottom-border"></view>
-		<template>
+		<template v-if="type==0">
 			<view class="form-input">
-				<input type="text" name="username" placeholder-class="input-placeholder" placeholder="邮箱/手机号">
+				<input type="text" name="username" placeholder-class="input-placeholder" v-model="username" placeholder="邮箱/手机号">
 			</view>
 			<view class="bottom-border"></view>
 			<view class="form-input password-input">
-				<input :type="inputType" style="flex:1" name="username" placeholder-class="input-placeholder" placeholder="密码">
+				<input :type="inputType" style="flex:1;border: none;font-size: 26rpx;outline: none;" name="password" v-model="password" placeholder-class="input-placeholder" placeholder="密码">
 				<iconfont :icon="look" :style="{color: lookColor}" @click.native="switchInputType"></iconfont>
 			</view>
+			<view class="bottom-border" style="margin-bottom: 20rpx;"></view>
+			<view :class="[loginBg?'login-btn login-btn-active':'login-btn login-btn-unactive']">登录</view>
 		</template>
-		<view class="bottom-border"></view>
-		<view class="login-btn">登录</view>
+		<template v-if="type==1">
+			<view class="form-input">
+				<input type="text" name="mobile" placeholder-class="input-placeholder" v-model="mobile" placeholder="手机号">
+			</view>
+			<view class="bottom-border"></view>
+			<view class="form-input password-input">
+				<input :type="inputType" style="flex:1;border: none;font-size: 26rpx;outline: none;" name="yzm" v-model="yzm" placeholder-class="input-placeholder" placeholder="输入验证码">
+				<text :style="{color: lookColor,fontSize:'26rpx'}" @click.native="switchInputType">获取验证码</text>
+			</view>
+			<view class="bottom-border" style="margin-bottom: 20rpx;"></view>
+			<view :class="[loginBg?'login-btn login-btn-active':'login-btn login-btn-unactive']">登录</view>
+		</template>
+		<view class="other-login" @click="switchLogin">其他登录方式</view>
 	</view>
 </template>
 
 <script>
-	import iconfont from '../../common/components/iconfont'
 	export default {
 		name:'login',
-		components:{
-			iconfont
-		},
 		data(){
 			return {
 				arrow:'\ue65b',
 				look:'\ue60b',
 				lookColor:'#DFDFDF',
-				inputType:'text'
+				inputType:'text',
+				username:'',
+				password:'',
+				mobile:'',
+				yzm:'',
+				type:1,
+			}
+		},
+		computed:{
+			loginBg(){
+				if(this.username && this.password){
+					return true
+				}
+				return false
 			}
 		},
 		methods:{
@@ -50,6 +72,13 @@
 			},
 			goBack(){
 				uni.navigateBack()
+			},
+			switchLogin(){
+				if(this.type == 1){
+					this.type = 0
+				}else if(this.type == 0){
+					this.type = 1
+				}
 			}
 		}
 	}
@@ -82,16 +111,19 @@
 		padding-top: 20rpx;
 		padding-bottom: 20rpx;
 	}
+
+	
 	.password-input{
 		display: flex;
 		align-items: center;
 	}
+	
 	.input-placeholder{
 		color: #D2D2D2;
 		font-size: 26rpx;
 	}
+	
 	.login-btn{
-		background-color: #EEEEEE;
 		width: 100%;
 		padding-top: 20rpx;
 		padding-bottom: 20rpx;
@@ -99,5 +131,20 @@
 		text-align: center;
 		font-size: 30rpx;
 		color: #A2A9B2;
+	}
+	
+	.login-btn-unactive{
+		background-color: #EEEEEE;
+	}
+	.login-btn-active{
+		background-color: $custom-font-color-blue;
+	}
+	
+	.other-login{
+		text-align: center;
+		margin-top: 20rpx;
+		color: $custom-font-color-blue;
+		font-weight: bold;
+		font-size: 26rpx;
 	}
 </style>
