c9ce1d292c8852aafe37affbc5040f7c789abcf9
lyqgit <lyqgit@email>
2020-11-02 15:25:02 +0800

    消息中心必须登录

--- pages/msgCenter/index.vue
+++ pages/msgCenter/index.vue
@@ -1,17 +1,17 @@
 <template>
 	<view class="msgCenter-layout">
 		<division height="30rpx"></division>
-		<myInfo title="评论" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=3')">
+		<myInfo title="评论" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=3',true)">
 			<text class="iconfont" style="color:#FFC347;">&#xe81a;</text>
 		</myInfo>
-		<myInfo title="点赞" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=1')">
+		<myInfo title="点赞" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=1',true)">
 			<text class="iconfont" style="color:#6CBD45;">&#xe672;</text>
 		</myInfo>
-		<myInfo title="关注" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=2')">
+		<myInfo title="关注" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=2',true)">
 			<text class="iconfont" style="color:#0076FF;">&#xe8bb;</text>
 		</myInfo>
 		<division height="30rpx"></division>
-		<myInfo title="系统消息" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=4')">
+		<myInfo title="系统消息" @click="goToPage('/pages/msgCenter/moduleMsg?messageType=4',true)">
 			<text class="iconfont" style="color:#0076FF;">&#xe6f2;</text>
 		</myInfo>
 	</view>
